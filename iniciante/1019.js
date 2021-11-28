a8f5005f6a277c0e7286b08c20e25896956e00a6
Rayane Pimentel <radharayg@gmail.com>
2021-11-28 17:46:05 -0300

    nova maneira de converter o tempo

--- iniciante/1019.js
+++ iniciante/1019.js
@@ -1,10 +1,13 @@
 let input = require('fs').readFileSync('stdin', 'utf-8');
 
 let valor = parseInt(input);
+let converter = [3600, 60, 1];
+let add = [];
 
-let horas = parseInt(valor / 3600);
-let resto = valor % 3600;
-let minutos = parseInt(resto / 60);
-let segundos = resto % 60;
+for (let qtdConvertida of converter) {
+  add.push(parseInt(valor / qtdConvertida));
+  valor = valor % qtdConvertida;
+}
 
-console.log(`${horas}:${minutos}:${segundos}`);
+
+console.log(add.join(":"));
